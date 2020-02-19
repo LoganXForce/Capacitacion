332362efdb1c60eb7f550d21cda218c8f8e9a4b9
ArmandoX99 <joan990511@hotmail.com>
2020-02-19 17:10:19 +0100

    Version 2

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/clientes', function(){
+    return view('clientes');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
